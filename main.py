c896ffa0827554f35786e8ddf6d9911f4ee47369
joaquin <mezzanojoaquin@gmail.com>
2025-04-29 13:12:39 -0300

    SerpAPI integration with Ollama

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 from langchain_ollama.llms import OllamaLLM
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.utilities import SerpAPIWrapper
+from langchain.agents import Tool, initialize_agent #Contruir y usar el agente
+from langchain.agents.agent_types import AgentType #Elegir el tipo de agente
+import getpass
+import os
 
+# Obtenemos API key de SerpAPI
+if not os.environ.get("SERPAPI_API_KEY"):
+    os.environ["SERPAPI_API_KEY"] = getpass.getpass("Enter API key for SERPAPI: ")
+
+# Configuramos herramienta de búsqueda para el agente
+search = SerpAPIWrapper()
+tools = [
+    Tool(
+        name="Search",
+        func=search.run, #Utiliza la función search de SerpAPI
+        description="Useful for answering questions about current events or factual data from the web." #Prompt interno del agente para decidir si usar esta herramienta o no.
+    )
+]
+
+# Modelo Ollama
 model = OllamaLLM(model="gemma2:2b")
 
-template = """
-    You are an educated teacher and chef with focus on Argentinian gastronomy.
+# Agente con Ollama + SerpAPI
+agent = initialize_agent(
+    tools=tools,
+    llm=model,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=True #Imprime el razonamiento del agente, sirve para debugear
+)
 
-    Here is the question to answer: {question}
-"""
-
-prompt = ChatPromptTemplate.from_template(template)
-chain = prompt | model
-
+# Loop interactivo
 while True:
-    question = input("Ask the Argentinian chef (q to quit): ")
+    question = input("Ask the Agent (q to quit): ")
     if question == "q": break
-    else:
-        result = chain.invoke({"question": question})
+    try:
+        result = agent.run(question)
         print(result)
+    except Exception as e:
+        print("Error: ", e)
